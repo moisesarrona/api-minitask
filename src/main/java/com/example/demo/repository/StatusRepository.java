4e050246bbdd9090632ff279678c3b2cb0ee77e0
Moises Arrona <57106162+MoisesArrona@users.noreply.github.com>
2022-01-18 21:49:31 -0600

    Borrando todo

--- src/main/java/com/example/demo/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusRepository extends JpaRepository<Status, Long> {
-}
