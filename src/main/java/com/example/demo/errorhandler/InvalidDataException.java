3f0f2259861552795ad34e1945f62207299731f7
moisesarrona <arronamoisesar@outlook.com>
2022-03-01 22:55:31 -0600

    Invocación de super clase
    Invocación de super clase

--- src/main/java/com/example/demo/errorhandler/InvalidDataException.java
+++ src/main/java/com/example/demo/errorhandler/InvalidDataException.java
@@ -8,10 +8,18 @@ import org.springframework.validation.BindingResult;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@SuperBuilder
 public class InvalidDataException extends  RuntimeException {
 
     private final transient BindingResult result;
+
+    public InvalidDataException(BindingResult result) {
+        super();
+        this.result = result;
+    }
+
+    public InvalidDataException(String message, BindingResult result) {
+        super(message);
+        this.result = result;
+    }
 
 }
