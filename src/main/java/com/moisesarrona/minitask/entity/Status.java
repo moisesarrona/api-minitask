fed3196e32253551cda84109e89f327cbbfe99a4
moisesarrona <arronamoisesar@outlook.com>
2022-05-12 23:07:06 -0500

    Delete ToString Add AllConstructor

--- src/main/java/com/moisesarrona/minitask/entity/Status.java
+++ src/main/java/com/moisesarrona/minitask/entity/Status.java
@@ -1,9 +1,9 @@
 package com.moisesarrona.minitask.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -19,7 +19,7 @@ import java.util.Date;
 @Setter
 @Getter
 @NoArgsConstructor
-@ToString
+@AllArgsConstructor
 @Entity
 @Table(name = "status")
 public class Status {
