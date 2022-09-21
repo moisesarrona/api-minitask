de404541597c3f96b6120ad1a571d6c2d8c31bb4
moisesarrona <arronamoisesar@outlook.com>
2022-09-21 10:49:05 -0500

    Refactor priority service

--- src/main/java/com/moisesarrona/minitask/entity/Priority.java
+++ src/main/java/com/moisesarrona/minitask/entity/Priority.java
@@ -1,46 +1,48 @@
 package com.moisesarrona.minitask.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 /**
  * @author moisesarrona
- * @version 0.1
+ * @version 0.0.2
  */
-@Setter
 @Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "priorities")
 public class Priority {
+    @Column(length = 50, nullable = false, unique = true)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
 
-    @NotEmpty(message = "Enter name")
-    @Size(min = 3, max = 50, message = "Minimum 3 wor Maximum 50")
-    @Column(length = 50, nullable = false, unique = true)
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Minimum 2 and Maximum 50 characters")
     private String name;
 
-    @Size(max = 255, message = "Maximum 255")
-    @Column(length = 255)
     private String description;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private Date created_at;
 
-    @UpdateTimestamp
+    @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private Date updated_at;
 }
