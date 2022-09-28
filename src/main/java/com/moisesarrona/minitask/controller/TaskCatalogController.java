a7e449766d660bdf162069e8f1bdbffe1b222f0d
moisesarrona <arronamoisesar@outlook.com>
2022-09-28 13:45:06 -0500

    Modify TaskCotalogController

--- src/main/java/com/moisesarrona/minitask/controller/TaskCatalogController.java
+++ src/main/java/com/moisesarrona/minitask/controller/TaskCatalogController.java
@@ -2,6 +2,7 @@ package com.moisesarrona.minitask.controller;
 
 import com.moisesarrona.minitask.entity.Phase;
 import com.moisesarrona.minitask.entity.Priority;
+import com.moisesarrona.minitask.entity.Tag;
 import com.moisesarrona.minitask.service.TaskCatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -54,15 +55,38 @@ public class TaskCatalogController {
 
     @RequestMapping(value = "/findPhaseByName/{name}")
     public ResponseEntity<List<Phase>> findPhaseByName(@PathVariable("name") String name) {
-        List<Phase> phaseDB = taskCatalogService.findPhasesByName(name);
-        if (phaseDB.isEmpty())
+        List<Phase> phasesDB = taskCatalogService.findPhasesByName(name);
+        if (phasesDB.isEmpty())
             return ResponseEntity.noContent().build();
-        return ResponseEntity.ok(phaseDB);
+        return ResponseEntity.ok(phasesDB);
     }
 
     @PostMapping(value = "/createdPhase")
     public ResponseEntity<Phase> createdPhase(@Valid @RequestBody Phase phase) {
         Phase phaseDB = taskCatalogService.createdPhase(phase);
         return ResponseEntity.ok(phaseDB);
+    }
+
+
+    @RequestMapping(value = "/findTagById/{id}")
+    public ResponseEntity<Tag> findTagById(@PathVariable("id") Long id) {
+        Tag tagDB = taskCatalogService.findTagById(id);
+        if (tagDB == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(tagDB);
+    }
+
+    @RequestMapping(value = "/findTagsByName/{name}")
+    public ResponseEntity<List<Tag>> findTagsByName(@PathVariable("name") String name) {
+        List<Tag> tagsDB = taskCatalogService.findTagsByName(name);
+        if (tagsDB.isEmpty())
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(tagsDB);
+    }
+
+    @PostMapping(value = "/createdTag")
+    public ResponseEntity<Tag> createdTag(@Valid @RequestBody Tag tag) {
+        Tag tagsDB = taskCatalogService.createdTag(tag);
+        return ResponseEntity.ok(tagsDB);
     }
 }
