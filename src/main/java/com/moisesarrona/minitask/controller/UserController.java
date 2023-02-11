fe0905aea76c6455ddb215f9ea69ff2a47a34917
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-02-11 01:12:12 -0600

    Migrate SpringBoot version from 2 to3

--- src/main/java/com/moisesarrona/minitask/controller/UserController.java
+++ src/main/java/com/moisesarrona/minitask/controller/UserController.java
@@ -2,6 +2,7 @@ package com.moisesarrona.minitask.controller;
 
 import com.moisesarrona.minitask.entity.User;
 import com.moisesarrona.minitask.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 /**
